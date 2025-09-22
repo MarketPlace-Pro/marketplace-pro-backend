c6fda99f6ae620135ab09a4a8d04ae301794ea97
MC <luckymasilo76@gmail.com>
2025-09-22 19:24:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to allow frontend to connect
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://marketplace-pro.github.io');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
