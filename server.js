6346a6d46987a565f482ef6987fa79c5fdd2a62c
MC <luckymasilo76@gmail.com>
2025-09-22 23:31:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,3 @@ app.get('/api/products/featured', (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
-// Start the server
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-});
